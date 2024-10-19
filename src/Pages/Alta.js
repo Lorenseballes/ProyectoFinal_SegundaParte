70cb27c836fe4ba52cd0be9f5b60681c546b180e
Loren Seballes <seballesloren@gmail.com>
2024-10-19 16:01:06 -0300

    se agregan detalles

--- src/Pages/Alta.js
+++ src/Pages/Alta.js
@@ -4,15 +4,15 @@ const Alta = () => {
         <form className="row g-3">
             <div className="col-10"> 
     <label for="nombre" className="form-label">Nombre</label>
-    <input type="text" className="form-control" id="inputAddress"/>
+    <input type="text" className="form-control" id="inputAddress" required/>
   </div>
   <div className="col-md-5">
     <label for="precio" className="form-label">Precio</label>
-    <input type="number" className="form-control" id="inputEmail4"/>
+    <input type="number" className="form-control" id="inputEmail4" required/>
   </div>
   <div className="col-md-5">
     <label for="stock" className="form-label">Stock</label>
-    <input type="number" className="form-control" id="inputPassword4"/>
+    <input type="number" className="form-control" id="inputPassword4" required/>
   </div>
   <div className="col-md-5">
     <label for="marca" className="form-label">Marca</label>
@@ -20,19 +20,19 @@ const Alta = () => {
   </div>
   <div className="col-md-5">
     <label for="categoria" className="form-label">Categoría</label>
-    <input type="text" className="form-control" id="inputPassword4"/>
+    <input type="text" className="form-control" id="inputPassword4" required/>
   </div>
   <div className="col-md-10">
     <label for="descripcion-corta" className="form-label">Descripción corta</label>
-    <input type="text" className="form-control" id="inputCity"/>
+    <input type="text" className="form-control" id="inputCity" required/>
   </div>
   <div className="col-md-10">
   <label for="descripcion-larga" class="form-label">Descripción larga</label>
-  <textarea className="form-control" id="descripcion" rows="3"></textarea>
+  <textarea className="form-control" id="descripcion" rows="3" required></textarea>
   </div>
   <div className="col-10">
     <div className="form-check">
-      <input className="form-check-input" type="checkbox" id="gridCheck"/>
+      <input className="form-check-input" type="checkbox" id="gridCheck" required/>
       <label className="form-check-label" for="gridCheck">
         Envío sin costo
       </label>
@@ -40,15 +40,15 @@ const Alta = () => {
   </div>
   <div className="col-md-5">
     <label for="edad-desde" className="form-label">Edad desde</label>
-    <input type="number" className="form-control" id="inputEmail4"/>
+    <input type="number" className="form-control" id="inputEmail4" required/>
   </div>
   <div className="col-md-5">
     <label for="edad-hasta" className="form-label">Edad hasta</label>
-    <input type="number" className="form-control" id="inputPassword4"/>
+    <input type="number" className="form-control" id="inputPassword4" required/>
   </div>
   <div className="col-md-5">
   <label for="imagen" className="form-label">Selecciona una foto</label>
-  <input class="form-control" type="file" id="formFile"/>
+  <input class="form-control" type="file" id="formFile" required/>
 </div>
   <div className="col-12">
     <button type="submit" className="btn btn-primary">Enviar</button>
--- src/Pages/Contacto.js
+++ src/Pages/Contacto.js
@@ -5,15 +5,15 @@ const Contacto = () => {
         <>
         <div className="col-6"> 
             <label for="nombre" className="form-label">Nombre</label>
-            <input type="text" className="form-control" id="inputAddress"/>
+            <input type="text" className="form-control" id="nombre" required/>
         </div>
         <div className="col-6"> 
             <label for="email" className="form-label">E-mail</label>
-            <input type="email" className="form-control" id="inputAddress"/>
+            <input type="email" className="form-control" id="email" required/>
         </div>
         <div className="col-md-6">
             <label for="comentarios" class="form-label">Comentarios</label>
-            <textarea className="form-control" id="descripcion" rows="3"></textarea>
+            <textarea className="form-control" id="descripcion" rows="3" required></textarea>
         </div>
         <div className="col-12">
             <button type="submit" className="btn btn-primary">Enviar</button>
